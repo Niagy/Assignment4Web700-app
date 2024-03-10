98a6ffbb955ebf6ead4366e7efb0aa9bb56094fb
John Niagwan <niagyn@yahoo.com>
2024-03-09 21:40:59 -0500

    Assignment4

---
+++ modules/collegeData.js
@@ -0,0 +1,142 @@
+const fs = require("fs");
+
+class Data{
+    constructor(students, courses){
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+let dataCollection = null;
+
+module.exports.initialize = function () {
+    return new Promise( (resolve, reject) => {
+        fs.readFile('./data/courses.json','utf8', (err, courseData) => {
+            if (err) {
+                reject("unable to load courses"); return;
+            }
+
+            fs.readFile('./data/students.json','utf8', (err, studentData) => {
+                if (err) {
+                    reject("unable to load students"); return;
+                }
+
+                dataCollection = new Data(JSON.parse(studentData), JSON.parse(courseData));
+                resolve();
+            });
+        });
+    });
+}
+
+module.exports.getAllStudents = function(){
+    return new Promise((resolve,reject)=>{
+        if (dataCollection.students.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(dataCollection.students);
+    })
+}
+
+module.exports.getTAs = function () {
+    return new Promise(function (resolve, reject) {
+        var filteredStudents = [];
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].TA == true) {
+                filteredStudents.push(dataCollection.students[i]);
+            }
+        }
+
+        if (filteredStudents.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(filteredStudents);
+    });
+};
+
+module.exports.getCourses = function(){
+   return new Promise((resolve,reject)=>{
+    if (dataCollection.courses.length == 0) {
+        reject("query returned 0 results"); return;
+    }
+
+    resolve(dataCollection.courses);
+   });
+};
+
+module.exports.getStudentByNum = function (num) {
+    return new Promise(function (resolve, reject) {
+        var foundStudent = null;
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].studentNum == num) {
+                foundStudent = dataCollection.students[i];
+            }
+        }
+
+        if (!foundStudent) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(foundStudent);
+    });
+};
+
+module.exports.getStudentsByCourse = function(course) {
+    return new Promise((resolve, reject) => {
+        if (dataCollection.students.length > 0) {
+            // Filter students by the given course
+            const studentsInCourse = dataCollection.students.filter(student => student.course === course);
+
+            if (studentsInCourse.length > 0) {
+                // If there are students in the course, resolve with the array
+                resolve(studentsInCourse);
+            } else {
+                // If no students found for the given course, reject with a meaningful message
+                reject(`No students found for course ${course}`);
+            }
+        } else {
+            // If no students or an empty array, reject with a meaningful message
+            reject("No results returned for getStudentsByCourse");
+        }
+    });
+}
+
+module.exports.getStudentByNum = function(num) {
+    return new Promise((resolve, reject) => {
+        // Check if dataCollection and students array exist
+        if (dataCollection.students.length > 0) {
+            // Find the student with the given studentNum
+            const student = dataCollection.students.find(student => student.studentNum === num);
+
+            if (student) {
+                // If a student is found, resolve with the student object
+                resolve(student);
+            } else {
+                // If no student found for the given studentNum, reject with a meaningful message
+                reject(`No student found with studentNum ${num}`);
+            }
+        } else {
+            // If no students or an empty array, reject with a meaningful message
+            reject("No results returned for getStudentByNum");
+        }
+    });
+}
+
+module.exports.addStudent = function(studentData) {
+    return new Promise((resolve, reject) => {
+        // If studentData.TA is undefined, set it to false, otherwise set it to true
+        studentData.TA = studentData.TA === undefined ? false : true;
+
+        // Set the studentNum property of studentData to be the length of the "dataCollection.students" array plus one
+        studentData.studentNum = dataCollection.students.length + 1;
+
+        // Push the updated studentData object onto the "dataCollection.students" array
+        dataCollection.students.push(studentData);
+
+        // Resolve the promise
+        resolve();
+    });
+};
---
+++ server.js
@@ -0,0 +1,127 @@
+/*********************************************************************************
+*  WEB700 – Assignment 04
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: John Niagwan Student ID: 121092225 Date: 09/03/2024
+*
+*  Online (Cycliic) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+var collegeData = require('./modules/collegeData');
+var path = require('path');
+app.use(express.static('images'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// Initialize collegeData
+collegeData.initialize()
+    .then(() => {
+        // Route to handle students
+        app.get("/students", (req, res) => {
+            const courseParam = req.query.course;
+
+            if (courseParam) {
+                collegeData.getStudentsByCourse(parseInt(courseParam))
+                    .then(students => {
+                        res.json(students);
+                    })
+                    .catch(() => {
+                        res.json({ message: 'no results' });
+                    });
+            } else {
+                collegeData.getAllStudents()
+                    .then(students => {
+                        res.json(students);
+                    })
+                    .catch(() => {
+                        res.json({ message: 'no results' });
+                    });
+            }
+        });
+
+        // Route to handle TAs
+        app.get("/tas", (req, res) => {
+            collegeData.getTAs()
+                .then(tas => {
+                    res.json(tas);
+                })
+                .catch(() => {
+                    res.json({ message: 'no results' });
+                });
+        });
+
+        // Route to handle courses
+        app.get("/courses", (req, res) => {
+            collegeData.getCourses()
+                .then(courses => {
+                    res.json(courses);
+                })
+                .catch(() => {
+                    res.json({ message: 'no results' });
+                });
+        });
+
+        // Route to handle a single student by num
+        app.get("/student/:num", (req, res) => {
+            const studentNum = parseInt(req.params.num);
+            collegeData.getStudentByNum(studentNum)
+                .then(student => {
+                    res.json(student);
+                })
+                .catch(() => {
+                    res.json({ message: 'no results' });
+                });
+        });
+
+        // Default route
+        app.get("/", (req, res) => {
+            res.sendFile(path.join(__dirname, '/views/home.html'));
+        });
+
+        // Route to handle about page
+        app.get("/about", (req, res) => {
+            res.sendFile(path.join(__dirname, '/views/about.html'));
+        });
+
+        // Route to handle HTML demo page
+        app.get("/htmlDemo", (req, res) => {
+            res.sendFile(path.join(__dirname, '/views/htmlDemo.html'));
+        });
+
+        app.post("/students/add", (req, res) => {
+                    collegeData.addStudent(req.body)
+                        .then(() => {
+                            res.redirect("/students");
+                        })
+                        .catch((err) => {
+                            // Handle error here
+                            console.log(err);
+                            res.redirect("/students/add");
+                        });
+                });
+
+        // Route to handle add student page
+         app.get("/students/add", (req, res) => {
+            res.sendFile(path.join(__dirname, '/views/addStudent.html'));
+        });
+
+        // Handling unmatched routes
+        app.use((req, res) => {
+            res.status(404).sendFile(path.join(__dirname, '/images/404.jpeg'));
+        });
+
+        // Start the HTTP server after initialization
+        app.listen(HTTP_PORT, () => {
+            console.log("Server listening on port: " + HTTP_PORT);
+        });
+    })
+    .catch(error => {
+        console.error(error);
+    });
